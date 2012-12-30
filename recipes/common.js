81ced06710923ac9b98598f50f5f9b3363d6d882
Philip Rideout <philiprideout@gmail.com>
2012-12-29 21:21:26 -0800

    experiment is working

--- recipes/common.js
+++ recipes/common.js
@@ -1,7 +1,5 @@
-
-// To find the "main" function definition, replace the HTML extension with JS.
-var thisPage = document.URL;
-var mainScript = thisPage.slice(0,-4) + "js"
+var cdn = "http://ajax.googleapis.com/ajax/libs/";
+var github = "http://github.com/prideout/book-webgl/blob/master/recipes/";
 
 // Use HeadJS to load scripts asynchronously, but execute them synchronously.
 head.js(
@@ -11,23 +9,34 @@ head.js(
   "../giza/Polygon.js",
   "../giza/Surface.js",
   "../giza/Turtle.js",
-  "../recipes/lib/demo.js",
-  "../recipes/lib/stats.min.js",
-  "http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js",
-  "http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js",
-  mainScript);
+  "lib/demo.js",
+  "lib/stats.min.js",
+  cdn + "jquery/1.8.0/jquery.min.js",
+  cdn + "jqueryui/1.9.2/jquery-ui.min.js");
 
 // After all scripts have been loaded AND after the document is "Ready", do this:
 head.ready(function() {
+
+  // Execute the recipe's main() function
   main();
-  var github = "https://github.com/prideout/book-webgl/blob/master/recipes";
-  var slash = mainScript.lastIndexOf("/");
-  var url = github + mainScript.slice(slash, -2);
-  var index = "../recipes/index.html";
+
+  // Add some github links into the button-bar element
+  var slash = document.URL.lastIndexOf("/");
+  var recipe = document.URL.slice(slash+1, -4);
+  var index = "index.html";
   $('#button-bar').html([
-    "<a href='" + index + "'>go to recipe list</a>",
-    "<a href='" + url + "html'>view HTML source</a>",
-    "<a href='" + url + "js'>view JavaScript source</a>",
+    "<a href='" + index + "'>",
+    "    go to recipe list",
+    "</a>",
+    "<a href='" + github + recipe + ".html'>",
+    "    view HTML source",
+    "</a>",
+    "<a href='" + github + recipe + ".js'>",
+    "    view JavaScript source",
+    "</a>",
   ].join('\n'));
+
+  // Tell jQueryUI to style the links as buttons
   $("a").button()
+
 });
